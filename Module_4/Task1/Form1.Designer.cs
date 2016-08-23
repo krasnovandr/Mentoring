598455b0ed6ec55c9eeb3dd9b9ee910947e22aeb
Andrei Krasnou <Andrei.Krasnou@easyjet.com>
2016-08-23 17:57:48 +0300

    module 4 update changes

--- Module_4/Task1/Form1.Designer.cs
+++ Module_4/Task1/Form1.Designer.cs
@@ -31,8 +31,14 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewButtonColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -62,38 +68,85 @@
             this.label1.TabIndex = 3;
             this.label1.Text = "URL";
             // 
-            // groupBox1
-            // 
-            this.groupBox1.AutoSize = true;
-            this.groupBox1.Location = new System.Drawing.Point(59, 71);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(567, 179);
-            this.groupBox1.TabIndex = 6;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
-            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(752, 71);
+            this.button2.Location = new System.Drawing.Point(722, 315);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 7;
-            this.button2.Text = "button2";
+            this.button2.Text = "Start";
             this.button2.UseVisualStyleBackColor = true;
-            //this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(59, 361);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(637, 96);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(722, 359);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(59, 315);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(637, 23);
+            this.progressBar1.TabIndex = 10;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(59, 71);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(518, 238);
+            this.dataGridView1.TabIndex = 11;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Url";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 370;
+            // 
+            // Column2
+            // 
+            this.Column2.FillWeight = 200F;
+            this.Column2.HeaderText = "Remove";
+            this.Column2.Name = "Column2";
+            this.Column2.Text = "Remove";
+            this.Column2.UseColumnTextForButtonValue = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(910, 262);
+            this.ClientSize = new System.Drawing.Size(910, 469);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -104,8 +157,13 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewButtonColumn Column2;
     }
 }
 
--- Module_4/Task1/Form1.cs
+++ Module_4/Task1/Form1.cs
@@ -1,90 +1,128 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 namespace Task1
 {
     public partial class Form1 : Form
     {
-        CancellationTokenSource cancellationToken;
-        private HttpClient client = new HttpClient();
-        private int row = 20;
-        private ProgressBar bar;
-        private List<Task> loads = new List<Task>();
+        readonly List<string> _urlList = new List<string>();
+        private CancellationTokenSource _cancellationToken;
+
         public Form1()
         {
             InitializeComponent();
-            Form.CheckForIllegalCrossThreadCalls = false;
         }
 
-        private async void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
-                Task task1 = Task.Factory.StartNew(() => {
-                    var progress = new Progress<double>();
-                    progress.ProgressChanged += Changed;
-                    cancellationToken = new CancellationTokenSource();
-                    var button = new Button();
-                    bar = new ProgressBar();
-                    var box = new TextBox();
-                    button.Location = new Point(461, row);
-                    button.Text = "Stop";
-                    bar.Location = new Point(344, row);
-                    box.Location = new Point(7, row);
-                    box.Text = textBox1.Text;
-                    button.Click += button_Click;
-                    groupBox1.Controls.Add(button);
-                    groupBox1.Controls.Add(bar);
-                    groupBox1.Controls.Add(box);
-                    row += 30;
-                    DownloadFileAsync("http://www.dotpdn.com/files/Paint.NET.3.5.11.Install.zip", progress,
-                        cancellationToken.Token);
-            });
-
-
-            //Task.WaitAll(loads.ToArray());
-
-
-            //cancellationToken = new CancellationTokenSource();
-            //try
-            //{
-            //    await DownloadFileAsync("http://www.dotpdn.com/files/Paint.NET.3.5.11.Install.zip", progress, cancellationToken.Token);
-            //}
-            //catch (OperationCanceledException ex)
-            //{
-            //    //resultsTextBox.Clear();
-            //}
-
+            var button = new Button();
+            dataGridView1.Rows.Add(textBox1.Text, button);
         }
 
-        void button_Click(object sender, EventArgs e)
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
         {
-            if (cancellationToken != null)
+            var senderGrid = (DataGridView)sender;
+
+            if (senderGrid.Columns[e.ColumnIndex] is DataGridViewButtonColumn &&
+                e.RowIndex >= 0)
             {
-                cancellationToken.Cancel();
+                senderGrid.Rows.RemoveAt(e.RowIndex);
             }
         }
 
-        private void Changed(object sender, double d)
+        private async void button2_Click_1(object sender, EventArgs e)
         {
-            bar.Value = (int)d;
+            _cancellationToken = new CancellationTokenSource();
+            richTextBox1.Text += "Downloads started." + Environment.NewLine;
+            dataGridView1.ClearSelection();
+           
+            MapGridColumnToTheList();
+            var progress = new Progress<double>();
+            progress.ProgressChanged += progress_ProgressChanged;
+
+            try
+            {
+                await Download(_cancellationToken.Token, progress);
+
+            }
+            catch (OperationCanceledException)
+            {
+                richTextBox1.Text += Environment.NewLine + "Downloads canceled." + Environment.NewLine;
+            }
+            catch (Exception)
+            {
+                richTextBox1.Text += Environment.NewLine + "Downloads failed." + Environment.NewLine;
+            }
+
+            _cancellationToken = null;
         }
 
-
-        private void button2_Click(object sender, EventArgs e)
+        private void MapGridColumnToTheList()
         {
-            if (cancellationToken != null)
+            _urlList.Clear();
+            foreach (DataGridViewRow dr in dataGridView1.Rows)
             {
-                cancellationToken.Cancel();
+                foreach (var cell in dr.Cells.OfType<DataGridViewTextBoxCell>())
+                {
+                    _urlList.Add(cell.Value.ToString());
+                }
             }
         }
 
-        public async Task DownloadFileAsync(string url, IProgress<double> progress, CancellationToken token)
+        private async Task Download(CancellationToken token, IProgress<double> progress)
         {
-            var response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead, token);
+
+            var client = new HttpClient();
+          
+            var downloadTasksQuery = _urlList.Select((t, i) => DownloadFileAsync(t, null, token, client, i)).ToList();
+
+            int finishedCount = default(int);
+            while (downloadTasksQuery.Any())
+            {
+                if (token.IsCancellationRequested)
+                {
+                    token.ThrowIfCancellationRequested();
+                }
+                Task<Result> firstFinishedTask = await Task.WhenAny(downloadTasksQuery);
+
+                downloadTasksQuery.Remove(firstFinishedTask);
+
+                var result = await firstFinishedTask;
+
+                finishedCount++;
+                dataGridView1.Rows[result.DwonloadNumber].Selected = true;
+                progress.Report((finishedCount * 1d) / (_urlList.Count * 1d) * 100);
+
+                richTextBox1.Text += string.Format("{0}Length of the download:  {1}", Environment.NewLine, result.Length);
+            }
+        }
+
+        void progress_ProgressChanged(object sender, double e)
+        {
+            progressBar1.Value = (int)e;
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (_cancellationToken != null)
+            {
+                _cancellationToken.Cancel();
+            }
+
+            progressBar1.Value = progressBar1.Minimum;
+        }
+
+        public async Task<Result> DownloadFileAsync(string url, IProgress<double> progress, CancellationToken token, HttpClient client, int number)
+        {
+
+
+            var response = await client.GetAsync(url, token);
 
             if (!response.IsSuccessStatusCode)
             {
@@ -96,40 +134,19 @@ namespace Task1
                 token.ThrowIfCancellationRequested();
             }
 
-            var total = response.Content.Headers.ContentLength.HasValue ? response.Content.Headers.ContentLength.Value : -1L;
-            var canReportProgress = total != -1 && progress != null;
+            byte[] urlContents = await response.Content.ReadAsByteArrayAsync();
 
-            using (var stream = await response.Content.ReadAsStreamAsync())
+            return new Result
             {
-                var totalRead = 0L;
-                var buffer = new byte[4096];
-                int read;
-                while ((read = await stream.ReadAsync(buffer, 0, buffer.Length, token)) != 0)
-                {
-                    // Was cancellation already requested? 
-
-                    if (token.IsCancellationRequested)
-                    {
-                        token.ThrowIfCancellationRequested();
-                    }
-
-                    totalRead += read;
-                    if (canReportProgress)
-                    {
-                        progress.Report((totalRead * 1d) / (total * 1d) * 100);
-                    }
-                }
-            }
+                Length = urlContents.Length,
+                DwonloadNumber = number
+            };
         }
+    }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
-
-        //private void button2_Click_1(object sender, EventArgs e)
-        //{
-        //    Task.WaitAll(loads.ToArray());
-        //}
+    public class Result
+    {
+        public int Length { get; set; }
+        public int DwonloadNumber { get; set; }
     }
 }
